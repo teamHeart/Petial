e330a461df76caba6010041a6934eede8b084207
Claire Watkins <33589916+PrncssClr@users.noreply.github.com>
2025-10-12 19:17:53 +0000

    fixing linting errors

--- src/Battle/battle_manager.gd
+++ src/Battle/battle_manager.gd
@@ -58,6 +58,8 @@ enum TurnState {
 	FLEEING
 }
 
+static var instance: BattleManager
+
 # Current high-level battle state (START, PLAYER_TURN, ENEMY_TURN, ...).
 @export var battle_state: int = BattleState.START
 
@@ -78,7 +80,6 @@ enum TurnState {
 @export var turn_order_queue_size: int = 11
 @export var turn_order_display_size: int = 9
 
-static var instance: BattleManager
 
 ## Runtime fields
 # The working queue of upcoming combatants (front is next to act).
