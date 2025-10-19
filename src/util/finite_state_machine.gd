51d4e903c7aa92cca78186c17233d1cdea1ab136
Claire Watkins <33589916+PrncssClr@users.noreply.github.com>
2025-10-19 20:58:35 +0000

    Refactor FiniteStateMachine and State classes: streamline state transition logic, enhance method calls for state processing, and improve signal handling

--- src/util/finite_state_machine.gd
+++ src/util/finite_state_machine.gd
@@ -4,7 +4,7 @@ extends Node
 ## A simple finite state machine implementation.
 ## States can be added to the `states` array, and transitions between states are managed.
 
-signal state_changed(new_state)
+signal state_changed(new_state)  #):
 
 @export var states: Array[State] = []
 
@@ -13,16 +13,45 @@ var previous_state: State = null
 
 
 func change_state(new_state: State) -> bool:  #():
+	var changed: bool = true
+	if not current_state:
+		if not new_state:
+			changed = false
+		if new_state not in states:
+			changed = false
+		current_state = new_state
+		current_state.enter()
+		emit_signal("state_changed", current_state)
+		changed = true
 	if not new_state or new_state == current_state:
-		return false
+		changed = false
 	if new_state not in states:
-		return false
+		changed = false
 	if current_state and not current_state.can_transition_to(new_state):
-		return false
+		changed = false
 	if current_state:
 		current_state.exit()
 	previous_state = current_state
 	current_state = new_state
 	current_state.enter()
 	emit_signal("state_changed", current_state)
-	return true
+	if changed:
+		if previous_state:
+			remove_child(previous_state)
+		add_child(current_state)
+	return changed
+
+
+func _process(_delta):
+	if current_state:
+		current_state.process(_delta)
+
+
+func _physics_process(_delta):
+	if current_state:
+		current_state.physics_process(_delta)
+
+
+func _input(_event):
+	if current_state:
+		current_state.input(_event)
