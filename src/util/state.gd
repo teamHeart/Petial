06658a0ec5e8201ca563a1762a17c20b7396eea5
Claire Watkins <33589916+PrncssClr@users.noreply.github.com>
2025-10-27 22:08:38 +0000

    Refactor state management in ChatBubble and enhance state transition handling

--- src/util/state.gd
+++ src/util/state.gd
@@ -4,24 +4,24 @@ extends Node
 ## A base class for states in a finite state machine.
 ## Each state can define valid transitions to other states.
 
-signal state_entered  #():
-signal state_exited  #():
+signal state_entered # ():
+signal state_exited # ():
 
 @export var valid_states: Array[State] = []
-var _on_enter: Callable = func() -> void: pass
-var _on_exit: Callable = func() -> void: pass
+var _on_enter: Callable = func(_prev_state: State) -> void: pass
+var _on_exit: Callable = func(_next_state: State) -> void: pass
 var _on_process: Callable = func(_delta: float) -> void: pass
 var _on_physics_process: Callable = func(_delta: float) -> void: pass
 var _on_input: Callable = func(_event: InputEvent) -> void: pass
 
 
-func enter():
-	_on_enter.call()
+func enter(_prev_state: State):
+	_on_enter.call(_prev_state)
 	emit_signal("state_entered")
 
 
-func exit():
-	_on_exit.call()
+func exit(_next_state: State):
+	_on_exit.call(_next_state)
 	emit_signal("state_exited")
 
 
